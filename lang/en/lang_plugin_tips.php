8f14fa4257180c0eefdd6c198bb0dd0a829e89da
Valery Votintsev <none@none>
2022-12-14 20:38:30 +0300

    Update EN language for the tips plugin
    --HG--
    branch : _multilang

--- lang/en/lang_plugin_tips.php
+++ lang/en/lang_plugin_tips.php
@@ -1,6 +1,17 @@
 <?php
 
+$lang = [
+//content/plugins/tips/tips_setting.php
+	'got_it'		=> 'Ok I know you got it',//'好的，我知道你知道了',
+	'tips_plugin'		=> 'Tips plugin',//'小贴士插件',
+	'tips_plugin_info'	=> 'This is the world\'s first emlog plug-in, it will send a warm reminder on your management page, the style is as follows.',//'这是世界上第一个emlog插件，它会在你的管理页面送上一句温馨的小提示，样式如下。',
+	'tips_plugin_info2'	=> 'In addition, the plug-in is also a demo, which can be modified on the basis of this plug-in to develop your own plug-in.',//'另外该插件也是一个demo，可以在这个插件基础上修改，开发出你自己的插件。',
+	'ok_i_know'		=> 'Okay, I know',//'好的，我知道了',
+];
+
+
 $array_tips = [
+//content/plugins/tips/tips.php
 	'In order to prevent the article from being lost, emlog will automatically save it for you when you write the article',
 	'You can save your unfinished articles in the draft box',
 	'Thumbnails will be automatically generated when large-size pictures are uploaded, thereby speeding up the page loading speed',
