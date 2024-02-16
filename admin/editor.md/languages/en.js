ba41af404065fee84e8d3e7ffea4d763072d7fa5
Valery Votintsev <vvotintsev@gmail.com>
2024-02-16 15:57:54 +0300

    Fix Editor.MD languages
    --HG--
    branch : _multilang

--- admin/editor.md/languages/en.js
+++ admin/editor.md/languages/en.js
@@ -100,95 +100,96 @@
                     title : "Help",
                     body  : `<div class=\"markdown-body\" style=\"font-family:Microsoft Yahei, Helvetica, Tahoma, STXihei,Arial;height:390px;overflow:auto;font-size:14px;border-bottom:1px solid #ddd;padding:0 20px 20px 0;\">
 				<h5>Markdown syntax tutorial</h5><ul>
-				<li><p><a href="target="_blank" https://daringfireball.net/projects/markdown/syntax" title="Markdown Syntax">John Gruber Markdown Syntax</a></p>
+				<li><p><a target="_blank" href="https://daringfireball.net/projects/markdown/syntax" title="Markdown Syntax">John Gruber Markdown Syntax</a></p>
 				</li><li><p><a target="_blank" href="https://guides.github.com/features/mastering-markdown/" title="Mastering Markdown">Mastering Markdown - Github guide</a></p>
 				</li><li><p><a target="_blank" href="https://help.github.com/articles/markdown-basics/" title="Markdown Basics">Markdown Basics from Github</a></p>
 				</li><li><p><a target="_blank" href="https://help.github.com/articles/github-flavored-markdown/" title="GitHub Flavored Markdown">GitHub Flavored Markdown</a></p>
 				</li><li><p><a target="_blank" href="http://www.markdown.cn/" title="Markdown syntax description (Simplified Chinese)">Markdown syntax description (Simplified Chinese)</a></p>
 				</li><li><p><a target="_blank" href="http://markdown.tw/" title="Markdown syntax description (Traditional Chinese)">Markdown syntax description (Traditional Chinese)</a></p>
 				</li></ul>
-				<h5 id="h5--keyboard-shortcuts-">Keyboard shortcuts</h5><blockquote>
+				<h5 id="h5--keyboard-shortcuts-">Keyboard shortcuts</h5>
+				<blockquote>
 				<p>Ctrl and Alt in the shortcut key table can be replaced by Cmd and Opt respectively on Mac systems.</p>
 				</blockquote>
 				<table>
 					<thead>
 					<tr>
-					<th style="text-align: center;"><strong><strong>Ctrl + S</strong></strong></th>
+					<th style="text-align: center;"><strong>Ctrl + S</strong></th>
 					<th style="text-align: center;">Save</th>
-					<th style="text-align: center;"><strong><strong>F9</strong></strong></th>
+					<th style="text-align: center;"><strong>F9</strong></th>
 					<th style="text-align: center;">Toggle live preview</th>
-					<th style="text-align: center;"><strong><strong>Ctrl + Shift + R</strong></strong></th>
+					<th style="text-align: center;"><strong>Ctrl + Shift + R</strong></th>
 					<th style="text-align: center;">Replace all</th>
 					</tr>
 					</thead>
 					<tbody>
 					<tr>
-					<td style="text-align: center;"><strong><strong>Ctrl+1~6</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl+1~6</strong></td>
 					<td style="text-align: center;">corresponds to H1 to H6 respectively</td>
-					<td style="text-align: center;"><strong><strong>F10</strong></strong></td>
+					<td style="text-align: center;"><strong>F10</strong></td>
 					<td style="text-align: center;">Editor full screen preview</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + D</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + D</strong></td>
 					<td style="text-align: center;">Current time</td>
 					</tr>
 					<tr>
-					<td style="text-align: center;"><strong><strong>Ctrl + U</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + U</strong></td>
 					<td style="text-align: center;">Unordered list</td>
-					<td style="text-align: center;"><strong><strong>While holding down the Ctrl key, select different places in the editing area</strong></strong></td>
+					<td style="text-align: center;"><strong>While holding down the Ctrl key, select different places in the editing area</strong></td>
 					<td style="text-align: center;">Multiple cursor selection</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + H</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + H</strong></td>
 					<td style="text-align: center;">Horizontal line</td>
 					</tr>
 					<tr>
-					<td style="text-align: center;"><strong><strong>Ctrl + B</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + B</strong></td>
 					<td style="text-align: center;">Bold</td>
-					<td style="text-align: center;"><strong><strong>Ctrl+ A</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl+ A</strong></td>
 					<td style="text-align: center;">Select all</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + L</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + L</strong></td>
 					<td style="text-align: center;">Link</td>
 					</tr>
 					<tr>
-					<td style="text-align: center;"><strong><strong>Ctrl + I</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + I</strong></td>
 					<td style="text-align: center;">italic</td>
-					<td style="text-align: center;"><strong><strong>Ctrl+ Z</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl+ Z</strong></td>
 					<td style="text-align: center;">Undo</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + Shift + A</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + Shift + A</strong></td>
 					<td style="text-align: center;">Github link</td>
 					</tr>
 					<tr>
-					<td style="text-align: center;"><strong><strong>Ctrl + K</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + K</strong></td>
 					<td style="text-align: center;">Inline code</td>
-					<td style="text-align: center;"><strong><strong>Ctrl+ Y</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl+ Y</strong></td>
 					<td style="text-align: center;">Redo</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + Shift + I</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + Shift + I</strong></td>
 					<td style="text-align: center;">Image</td>
 					</tr>
 					<tr>
-					<td style="text-align: center;"><strong><strong>Shift + Alt + L</strong></strong></td>
+					<td style="text-align: center;"><strong>Shift + Alt + L</strong></td>
 					<td style="text-align: center;">Convert selected text to lowercase</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + F</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + F</strong></td>
 					<td style="text-align: center;">Find search</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + Shift + C</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + Shift + C</strong></td>
 					<td style="text-align: center;">Code block</td>
 					</tr>
 					<tr>
-					<td style="text-align: center;"><strong><strong>Shift + Alt+ U</strong></strong></td>
+					<td style="text-align: center;"><strong>Shift + Alt+ U</strong></td>
 					<td style="text-align: center;">Convert first letter to uppercase</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + Shift + G</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + Shift + G</strong></td>
 					<td style="text-align: center;">Previous result</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + Shift + P</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + Shift + P</strong></td>
 					<td style="text-align: center;">Preformatted code block</td>
 					</tr>
 					<tr>
-					<td style="text-align: center;"><strong><strong>Ctrl + Alt + G</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + Alt + G</strong></td>
 					<td style="text-align: center;">Jump to line</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + G</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + G</strong></td>
 					<td style="text-align: center;">Next result</td>
-					<td style="text-align: center;"><strong><strong>Ctrl + Shift + H</strong></strong></td>
+					<td style="text-align: center;"><strong>Ctrl + Shift + H</strong></td>
 					<td style="text-align: center;">Html real font characters</td>
 					</tr>
 					</tbody>
-					</table>
-				</div>`
+				</table>
+			    </div>`
                 }
             }
         };
